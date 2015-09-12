3705563a54175d8d3643f8d2f163570b6455ecaa
Fernando Blat <ferblape@gmail.com>
2015-09-12 12:22:51 +0200

    New directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 lock '3.4.0'
 
-set :application, 'loquehicimos.populate.tools'
+set :application, 'loquehicimos'
 set :repo_url, 'git@github.com:PopulateTools/wikidoc.git'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
